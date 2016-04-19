596c14d92fa5ebeb8f3ea2dfddbd9c3e895a28ca
Kelly Burrowes <k.burrowes@auckland.ac.nz>
2016-04-19 03:39:57 +0000

    Added capillaryflow.f90 solver file. This contains all capillary flow related code.

--- src/other_consts.f90
+++ src/other_consts.f90
@@ -13,7 +13,15 @@ module other_consts
   use arrays,only: dp
   implicit none
 
+  integer :: num_symm_gen
+
   real(dp), parameter :: PI = 3.14159265358979_dp
+  real(dp) :: Palv,sheet_h0,K_cap,F_cap,F_sheet,sigma_cap,mu_c,alpha_c,area_scale
+  real(dp) :: F_rec,sigma_rec,L_c,Plb_c,Pub_c,Pub_a_v,total_cap_area,L_art_terminal
+  real(dp) :: H0,L_scale,L_vein_terminal,Ptm_max,R_art_terminal,R_vein_terminal
+  real(dp) :: INITIAL_LPM,mu_app(13)
+  real(dp) :: L_a(13),L_v(13),rad_a(13),rad_v(13),alpha_a,alpha_v
+
   logical :: diagnostics_on
   logical :: test_cpu_time
 
