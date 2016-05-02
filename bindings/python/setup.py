d7544f302023a1294ab1f9300f09dad7805c6d78
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-05-02 03:11:17 +0000

    Add setup.py script for using built Python bindings.

---
+++ bindings/python/setup.py
@@ -0,0 +1,39 @@
+""" Aether library - routines for a modelling the lung
+
+The Aether library is an advanced modelling library for models of the lung.
+"""
+
+classifiers = """\
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+Intended Audience :: Education
+Intended Audience :: Science/Research
+License :: OSI Approved :: Apache Software License
+Programming Language :: Python
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.5
+Operating System :: Microsoft :: Windows
+Operating System :: Unix
+Operating System :: MacOS :: MacOS X
+Topic :: Scientific/Engineering :: Medical Science Apps.
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+from setuptools import setup
+
+doclines = __doc__#.split("\n")
+
+setup(
+	name='aether',
+	version='0.1.0',
+	author='H. Sorby',
+	author_email='h.sorby@auckland.ac.nz',
+	packages=['aether'],
+#	package_data={'aether': []},
+	platforms=['any'],
+	url='https://lung.bioeng.auckland.ac.nz/',
+	license='http://www.apache.org/licenses/LICENSE-2.0',
+	description='Aether library of routines for modelling the lung.',
+	classifiers = filter(None, classifiers.split("\n")),
+	long_description=doclines,
+)
