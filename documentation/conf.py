119599b95dab9fd1989fed44fb5ccb4f6afd5cf7
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-05-30 12:12:38 +1200

    Add fork me button to sidebar of documentation.

--- documentation/conf.py
+++ documentation/conf.py
@@ -167,6 +167,9 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
+html_sidebars = {
+   '**': ['localtoc.html', 'forksidebar.html', 'sourcelink.html', 'searchbox.html'],
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
@@ -182,7 +185,7 @@ html_static_path = ['_static']
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
