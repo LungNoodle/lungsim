8c7392a78af777d1adb4ddc1e44341604ff95b9f
Alys Clark <alys.clark@auckland.ac.nz>
2017-06-13 10:28:32 +1200

    Added structure to parameters defining inputs from tissue mechanics

--- src/pressure_resistance_flow.f90
+++ src/pressure_resistance_flow.f90
@@ -48,7 +48,7 @@ contains
     logical :: ADD=.FALSE.,CONVERGED=.FALSE.
     character(len=60) :: sub_name,mesh_type,vessel_type,mechanics_type,bc_type,grav_type
     integer :: grav_dirn,no,depvar,KOUNT,nz,ne,SOLVER_FLAG
-    real(dp) :: MIN_ERR,N_MIN_ERR,ptrans,elasticity_parameters(3),grav_factor,pleural_density
+    real(dp) :: MIN_ERR,N_MIN_ERR,elasticity_parameters(3),mechanics_parameters(2),grav_factor
 
     sub_name = 'evaluate_prq'
     call enter_exit(sub_name,1)
@@ -57,15 +57,19 @@ contains
 !
 !
 !vessel_type:
-!rigid, no elasticity, no parameters required
-!elastic_g_elasticity_parameters(2), R=R0*((Ptm/G0)+1.d0)^(1.d0/elasticity_parameters(2)),with an optional maximum pressure beyond which the vessel radius is constant three parameters, g0, elasticity_parameters(2), elasticity_parameters(3)
-!elastic alpha,  R=R0*(alpha*Ptm+1.d0), up to a limit elasticity_parameters(3) two parameters alpha, elasticity_parameters(3)
-!elastic_hooke, two parameters E and h,R=R0+3.0_dp*R0**2*Ptm/(4.0_dp*E*h*R0)
+  !rigid, no elasticity, no parameters required
+  !elastic_g0_beta, R=R0*((Ptm/G0)+1.d0)^(1.d0/elasticity_parameters(2)),with an optional maximum pressure beyond which the vessel radius is constant three parameters, g0, elasticity_parameters(2), elasticity_parameters(3)
+  !elastic alpha,  R=R0*(alpha*Ptm+1.d0), up to a limit elasticity_parameters(3) two parameters alpha, elasticity_parameters(3)
+  !elastic_hooke, two parameters E and h,R=R0+3.0_dp*R0**2*Ptm/(4.0_dp*E*h*R0)
 
-!mechanics type: const (constant pressure external to vessels), linear (assumed gradient in Ppl along gravitational direction), mechanics (Ppl determined by solution to a mechanics model)
+!mechanics type:
+  !linear two parmeters, transpulmonary pressure (average) and pleural density (gradient)
+  !mechanics, two parameters, pressure and stretch fields
+
+
 !bc_type: can be pressure (at inlet and outlets) or flow (flow at inlet pressure at outlet).
 mesh_type='simple_tree'
-vessel_type='elastic_hooke'
+vessel_type='elastic_g0_beta'
 mechanics_type='linear'
 bc_type='pressure'
 grav_type='on'
@@ -85,9 +89,18 @@ elseif (vessel_type.eq.'elastic_hooke') then
      elasticity_parameters(2)=0.1_dp!this is a fraction of the radius so is unitless
      elasticity_parameters(3)=0.0_dp !Not used
 else
-    print *, 'Your chosen vessel type does not seem to be implemented assuming rigid'
+    print *, 'WARNING: Your chosen vessel type does not seem to be implemented assuming rigid'
     vessel_type='rigid'
     elasticity_parameters=0.0_dp
+endif
+
+if (mechanics_type.eq.'linear') then
+    mechanics_parameters(1)=5.33_dp*98.07_dp !average transpulmonary pressure (Pa)
+    mechanics_parameters(2)=0.25_dp*0.1e-5_dp !pleural density, defines gradient in pleural pressure (kg/mm**3)
+else
+    print *, 'WARNING: Only linear mechanics models have been implemented to date,assuming default parameters'
+        mechanics_parameters(1)=5.33_dp*98.07_dp !average transpulmonary pressure (Pa)
+    mechanics_parameters(2)=0.25_dp*0.1e-5_dp !pleural density, defines gradient in pleural pressure (kg/mm**3)
 endif
 
 grav_dirn=2
@@ -113,8 +126,6 @@ endif
 
 inletbc=2266.0_dp
 outletbc=666.7_dp
-ptrans=5.33_dp
-pleural_density=0.25_dp*0.1e-5_dp !kg/mm**3
 
 !!---------DESCRIPTION OF IMPORTANT PARAMETERS-----------
 !viscosity: fluid viscosity
@@ -250,7 +261,7 @@ pleural_density=0.25_dp*0.1e-5_dp !kg/mm**3
       else
 !Update vessel radii based on predicted pressures and then update resistance through tree
         call calc_press_area(grav_type,grav_vect,KOUNT,depvar_at_node,prq_solution,&
-           mesh_dof,ptrans,vessel_type,elasticity_parameters,pleural_density)
+           mesh_dof,vessel_type,elasticity_parameters,mechanics_type,mechanics_parameters)
         call calculate_resistance(viscosity,density,gamma)
 
 !Put the ladder stuff here --> See solve11.f
@@ -865,18 +876,19 @@ subroutine calc_sparse_size(mesh_dof,depvar_at_elem,depvar_at_node,FIX,NonZeros,
 !*calc_press_area:* Calculates new radii based on pressure area relnships
 
 subroutine calc_press_area(grav_type,grav_vect,KOUNT,depvar_at_node,prq_solution,&
-    mesh_dof,ptrans,vessel_type,elasticity_parameters,pleural_density)
+    mesh_dof,vessel_type,elasticity_parameters,mechanics_type,mechanics_parameters)
 
     use indices
     use arrays,only: dp,num_nodes,num_elems,elem_field,elem_nodes,node_xyz
     use diagnostics, only: enter_exit
     character(len=60), intent(in) :: grav_type
     character(len=60), intent(in) :: vessel_type
+    character(len=60), intent(in) :: mechanics_type
     real(dp), intent(in) :: grav_vect(3)
     integer,intent(in) :: KOUNT,mesh_dof
     integer,intent(in) :: depvar_at_node(num_nodes,0:2,2)
     real(dp),intent(in) ::  prq_solution(mesh_dof,2)
-    real(dp),intent(in) :: elasticity_parameters(3),ptrans,pleural_density
+    real(dp),intent(in) :: elasticity_parameters(3),mechanics_parameters(2)
 
 !local variables
     integer :: nj,np,ne,ny,nn
@@ -900,10 +912,10 @@ subroutine calc_press_area(grav_type,grav_vect,KOUNT,depvar_at_node,prq_solution
       if(grav_type.eq.'on')then
         do nj=1,3
           HEIGHT(nj)=node_xyz(nj,np)-node_xyz(nj,1) !ARC - where to put grav reference height?
-          G_PLEURAL=G_PLEURAL+PLEURAL_DENSITY*grav_vect(nj)*9810.d0*HEIGHT(nj) !kg
+          G_PLEURAL=G_PLEURAL+mechanics_parameters(2)*grav_vect(nj)*9810.d0*HEIGHT(nj) !kg
        enddo
       endif
-      Ppl=(ptrans*98.07d0)-G_PLEURAL !cmH2O->Pa
+      Ppl=mechanics_parameters(1)-G_PLEURAL !Pa
       Pblood=prq_solution(ny,1)! Pa
       Ptm=Pblood+Ppl     ! Pa
       R0=elem_field(ne_radius_in0,ne)
