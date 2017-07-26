4d42a673e1515fbb8c2db346f1570b280b0eadb9
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-07-26 13:05:01 +1200

    Mark QT_QMAKE_EXECUTABLE as advanced.

--- cmake/miscfunctions.cmake
+++ cmake/miscfunctions.cmake
@@ -3,6 +3,7 @@ function(TIDY_GUI)
     # Hide some variables in the GUI
     mark_as_advanced(CMAKE_CODEBLOCKS_EXECUTABLE)
     mark_as_advanced(CMAKE_CODEBLOCKS_EXECUTABLE)
+    mark_as_advanced(QT_QMAKE_EXECUTABLE)
     if (APPLE)
         mark_as_advanced(CMAKE_OSX_ARCHITECTURES)
         mark_as_advanced(CMAKE_OSX_DEPLOYMENT_TARGET)
