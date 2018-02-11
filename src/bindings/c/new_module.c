eaa07719b1499ad366fdd2a797043373df5cd798
Yuwen Zhang <yzha947@bn332842.bioeng.auckland.ac.nz>
2018-02-12 11:06:29 +1300

    delete new module

--- src/bindings/c/new_module.c
+++
@@ -1,9 +0,0 @@
-
-#include "new_module.h"
-
-void test_function();
-
-void test_function()
-{
-  test_function_c();
-}
--- src/bindings/c/new_module.h
+++
@@ -1,8 +0,0 @@
-#ifndef AETHER_NEW_MODULE_H
-#define AETHER_NEW_MODULE_H
-
-#include "symbol_export.h"
-
-SHO_PUBLIC void test_function();
-
-#endif /* AETHER_NEW_MODULE_H */
