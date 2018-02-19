0ab052bdac761247988a845eed791f96f004b57a
Alys Clark <alys.clark@auckland.ac.nz>
2018-02-19 21:00:25 +1300

    Reading in perfusion results for gas exhange

--- src/bindings/c/imports.c
+++ src/bindings/c/imports.c
@@ -4,10 +4,16 @@
 
 #include <string.h>
 
-void import_terminal_ventilation_c(const char *FLOWFILE,int *FLOWFILE_LEN);
+void import_ventilation_c(const char *FLOWFILE,int *FLOWFILE_LEN);
+void import_perfusion_c(const char *FLOWFILE,int *FLOWFILE_LEN);
 
-void import_terminal_ventilation(const char *FLOWFILE)
+void import_ventilation(const char *FLOWFILE)
 {
 	int filename_len = strlen(FLOWFILE);
-	import_terminal_ventilation_c(FLOWFILE, &filename_len);
+	import_ventilation_c(FLOWFILE, &filename_len);
+}
+void import_perfusion(const char *FLOWFILE)
+{
+	int filename_len = strlen(FLOWFILE);
+	import_perfusion_c(FLOWFILE, &filename_len);
 }
--- src/bindings/c/imports.h
+++ src/bindings/c/imports.h
@@ -3,6 +3,7 @@
 
 #include "symbol_export.h"
 
-SHO_PUBLIC void import_terminal_ventilation(const char *FLOWFILE);
+SHO_PUBLIC void import_ventilation(const char *FLOWFILE);
+SHO_PUBLIC void import_perfusion(const char *FLOWFILE);
 
 #endif /* AETHER_IMPORTS_H */
