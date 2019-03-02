633c39e884ee5ed7d8dab78834f8642a2b5ccd5f
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-03-02 03:53:04 +0000

    Improve the find package superlu script to create a superlu target.

--- cmake/FindSuperLU.cmake
+++ cmake/FindSuperLU.cmake
@@ -12,13 +12,29 @@ find_path(SUPERLU_INCLUDES
   PATHS
   $ENV{SUPERLUDIR}
   ${INCLUDE_INSTALL_DIR}
+  ${SUPERLU_INSTALL_DIR}
   PATH_SUFFIXES
   superlu
+  include/superlu
   SRC
 )
 
+if (SuperLU_FIND_VERSION_EXACT)
+  set(_SUPERLU_KNOWN_VERSIONS ${SuperLU_FIND_VERSION})
+else()
+  set(_SUPERLU_KNOWN_VERSIONS ${SuperLU_FIND_VERSION} "5.2.1" "5.2" "5.1.1" "5.1" "5.0" "4.3" "4.2" "4.2" "4.0" "3.1" "3.0")
+endif()
+
+set(_SUPERLU_LIBRARY_NAMES)
+foreach(_VERSION ${_SUPERLU_KNOWN_VERSIONS})
+  list(APPEND _SUPERLU_LIBRARY_NAMES "superlu_${_VERSION}" "superlu-${_VERSION}")
+endforeach()
+list(APPEND _SUPERLU_LIBRARY_NAMES "superlu")
+unset(_VERSION)
+unset(_SUPERLU_KNOWN_VERSIONS)
+
 find_library(SUPERLU_LIBRARIES
-  NAMES "superlu_5.2.1" "superlu_5.2" "superlu_5.1.1" "superlu_5.1" "superlu_5.0" "superlu_4.3" "superlu_4.2" "superlu_4.1" "superlu_4.0" "superlu_3.1" "superlu_3.0" "superlu"
+  NAMES ${_SUPERLU_LIBRARY_NAMES}
   PATHS $ENV{SUPERLUDIR} ${LIB_INSTALL_DIR}
   PATH_SUFFIXES lib)
 
@@ -96,5 +112,33 @@ find_package_handle_standard_args(SUPERLU
 
 mark_as_advanced(SUPERLU_INCLUDES SUPERLU_LIBRARIES)
 
+if(SUPERLU_FOUND)
+  if (NOT TARGET superlu)
+    if (NOT CMAKE_CFG_INTDIR STREQUAL .)
+        string(TOUPPER ${CMAKE_CFG_INTDIR} _CURRENT_BUILD_TYPE)
+    elseif(CMAKE_BUILD_TYPE)
+        string(TOUPPER ${CMAKE_BUILD_TYPE} _CURRENT_BUILD_TYPE)
+    else()
+        set(_CURRENT_BUILD_TYPE NOCONFIG)
+    endif()
+    set(_FIRST_LIB ${SUPERLU_LIBRARIES})
+    # I'm not aware of a framework for SuperLU but just in case.
+    # Treat apple frameworks separate
+    # See http://stackoverflow.com/questions/12547624/cant-link-macos-frameworks-with-cmake
+    if(APPLE AND ${_FIRST_LIB} MATCHES ".framework$")
+        string(REGEX REPLACE ".*/([A-Za-z0-9.]+).framework$" "\\1" FW_NAME ${_FIRST_LIB})
+        #message(STATUS "Matched '${FW_NAME}' to ${LIB}")
+        set(_FIRST_LIB "${_FIRST_LIB}/${FW_NAME}")
+    endif()
 
+    add_library(superlu UNKNOWN IMPORTED)
+    set_target_properties(superlu PROPERTIES
+      IMPORTED_LOCATION_${_CURRENT_BUILD_TYPE} ${_FIRST_LIB}
+      IMPORTED_CONFIGURATIONS ${_CURRENT_BUILD_TYPE}
+      INTERFACE_INCLUDE_DIRECTORIES ${SUPERLU_INCLUDES}
+    )
 
+    unset(_CURRENT_BUILD_TYPE)
+    unset(_FIRST_LIB)
+  endif()
+endif()
