680f42bdaba12e8d1eb04f48dc3f19e05b49de0c
Behdad Shaarbaf Ebrahimi <behdad.ebrahimi@gmail.com>
2019-04-26 16:26:47 +1200

    Sparse column and row storage and constants calculation added and finalised

--- src/lib/capillaryflow.f90
+++ src/lib/capillaryflow.f90
@@ -175,8 +175,6 @@ write (*,*) 'C_3 = ',RHS2(N_Nodes+1),'+ i(',RHS2(2*N_Nodes+2),')'
   write ( *, '(a)' ) ''
 
 
-pause
-
 end subroutine calc_cap_imped
 !
 !###################################################################################
@@ -459,15 +457,12 @@ end subroutine Mat_to_CSR
     integer :: MatrixSize,NonZeros,submatrixsize,ngen,i
     real(dp) :: area,Q01_mthrees,sheet_number
     character(len=60) :: sub_name
-    real(dp) :: ha,hv,omega
+
     
     sub_name = 'cap_flow_ladder'
     call enter_exit(sub_name,1)
 
-    ha = 2.0
-    hv = 1.0
-    omega = 10.0
-    call calc_cap_imped(ha,hv,omega)
+    
 !     Number of non-zero entries in solution matrix. 
       NonZeros=3
       do i=2,cap_param%num_symm_gen
