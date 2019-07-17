f4818bf1cf15967416154291b651d7ab6ef922f7
Alys Clark <alys.clark@auckland.ac.nz>
2019-07-17 15:59:06 +1200

    adding pycharm files to gitignore in case anyone uses it

--- src/bindings/c/exports.h
+++ src/bindings/c/exports.h
@@ -11,6 +11,7 @@ SHO_PUBLIC void export_node_field(int nj_field, const char *EXNODEFIELD, const c
 SHO_PUBLIC void export_terminal_solution(const char *EXNODEFILE, const char *name);
 SHO_PUBLIC void export_terminal_perfusion(const char *EXNODEFILE, const char *name);
 SHO_PUBLIC void export_terminal_ssgexch(const char *EXNODEFILE, const char *name);
+SHO_PUBLIC void export_node_geometry(const char *EXNODEFILE, const char *name);
 SHO_PUBLIC void export_node_geometry_2d(const char *EXNODEFILE, const char *name, int offset);
 SHO_PUBLIC void export_data_geometry(const char *EXDATAFILE, const char *name, int offset);
 SHO_PUBLIC void export_elem_field(const char *EXELEMFIELD, const char *name, const char *field_name);
