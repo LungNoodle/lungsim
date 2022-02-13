32ff4285ab3ec88908bef6745a4284b3f9242890
hsorby <h.sorby@auckland.ac.nz>
2022-02-13 13:27:02 +1300

    Add script to find latest oneAPI compiler environment script.

---
+++ .github/scripts/oneapi-env-vars-location.bat
@@ -0,0 +1,4 @@
+for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
+echo "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
+echo "::set-output name=env_bat::C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
+
