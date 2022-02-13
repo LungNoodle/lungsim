c54e9d995fbbc8b3042746599ce3a51ff3a3ce71
hsorby <h.sorby@auckland.ac.nz>
2022-02-13 15:25:53 +1300

    GitHub actions change 49.

--- .github/scripts/oneapi-env-vars-location.bat
+++ .github/scripts/oneapi-env-vars-location.bat
@@ -1,6 +1,5 @@
 @echo off
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
-echo %LATEST_VERSION%
-REM echo "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
+echo "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
 REM echo "::set-output name=env_bat::C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
 
