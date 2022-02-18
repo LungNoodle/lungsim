d8bae0249088b9015d6b3bbdcada4e6d2b70a95d
hsorby <h.sorby@auckland.ac.nz>
2022-02-18 22:23:14 +1300

    Correct syntax error in __init__.py.

--- src/bindings/python/__init__.py
+++ src/bindings/python/__init__.py
@@ -5,7 +5,7 @@ import sys
 
 if platform.system() == "Windows":
     version_info = sys.version_info
-    if sys.version_info.major == 3 && sys.version_info.minor < 8:
+    if sys.version_info.major == 3 and sys.version_info.minor < 8:
         sys.path.append(os.path.join(sys.prefix, "Library", "bin"))
     else:
         os.add_dll_directory(os.path.join(sys.prefix, "Library", "bin"))
