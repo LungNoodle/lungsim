7ba445bf3e1c93d8cc5564d13396eef80f21ed9a
Merryn Tawhai <mhow008@BN411727.hub>
2022-08-19 13:26:00 +1200

    removing default arrays for defining parameters from arrays.f90 and update parameters from bindings and lib

--- src/bindings/c/arrays.c
+++ src/bindings/c/arrays.c
@@ -3,16 +3,10 @@
 #include "string.h"
 
 void set_node_field_value_c(int *row, int *col, double *value);
-void update_parameter_c(const char *parameter_name, int *parameter_name_len, double *parameter_value);
 
 void set_node_field_value(int row, int col, double value)
 {
   set_node_field_value_c(&row, &col, &value);
 }
 
-void update_parameter(const char *parameter_name, double parameter_value)
-{
-  int parameter_name_len = (int)strlen(parameter_name);
-  update_parameter_c(parameter_name, &parameter_name_len, &parameter_value);
-}
 
